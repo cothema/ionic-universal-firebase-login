146377a14d21aae204cf77c26e7d1a952967a5fa
Miloš Havlíček <miloshavlicek@gmail.com>
2020-02-02 18:14:44 +0100

    prepare for service tests

---
+++ src/test.spec.ts
@@ -0,0 +1,9 @@
+import { TestBed } from "@angular/core/testing";
+
+describe("Example test", () => {
+    beforeEach(() => TestBed.configureTestingModule({}));
+
+    it("test", () => {
+        expect(true).toBeTruthy();
+    });
+});
